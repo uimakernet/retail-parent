a4a4dc7f3e4bdecb1d742b0dce92c8ad7f6a70a8
levent8421 <levent8421@outlook.com>
2019-05-16 10:07:07 +0800

    Initial commit

---
+++ retail-commons/src/test/java/club/xyes/zkh/retailcommons/RetailCommonsApplicationTests.java
@@ -0,0 +1,16 @@
+package club.xyes.zkh.retailcommons;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetailCommonsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ retail-web/retail-web-front/src/main/java/club/xyes/zkh/retail/web/front/RetailWebFrontApplication.java
@@ -0,0 +1,13 @@
+package club.xyes.zkh.retail.web.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailWebFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailWebFrontApplication.class, args);
+    }
+
+}
